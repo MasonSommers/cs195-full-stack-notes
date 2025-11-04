2c465d1cf0400e2a3e147f4628c54600b9b3929f
Meredith <meredith.moore@drake.edu>
2025-11-04 13:00:16 -0600

    updated exercise

--- day16-useEffect/useEffect-demos/src/demos/Exercise-EscapeDetector.jsx
+++ day16-useEffect/useEffect-demos/src/demos/Exercise-EscapeDetector.jsx
@@ -3,18 +3,8 @@ import { useState, useEffect } from "react";
 function EscapeDetector() {
   const [pressed, setPressed] = useState(false);
 
-  useEffect(() => {
-    function handleKeyDown(event) {
-      if (event.key === "Escape") {
-        setPressed(true);
-      }
-    }
-
-    window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, []);
+  // TODO: Add your useEffect here
 
   return <div>{pressed ? "✅ Escape pressed!" : "⌨️ Press Escape..."}</div>;
 }
-
 export default EscapeDetector;
